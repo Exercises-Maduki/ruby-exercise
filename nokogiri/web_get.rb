90dbc9dd4729322642af6dcd0d9ecb1a65133ab2
Amanda DuKawa <110678035+madukisp@users.noreply.github.com>
2023-12-10 05:32:04 -0300

    nokogiri

---
+++ nokogiri/web_get.rb
@@ -0,0 +1,10 @@
+#requisições http
+#get - "pegar" informações na web
+
+require 'net/http'
+
+exemplo = Net::HTTP.get('example.com', '/index.html')
+
+File.open('example.html', 'w') do |line|
+  line.puts(exemplo)
+end
